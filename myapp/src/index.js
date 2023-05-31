76cf8230008dd94488f6825e1af6cd7bad293ccf
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 17:49:15 +0100

    Event Object

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -14,9 +14,22 @@ function Football() {
     <button onClick={() => shoot("Goal!")}>Take the shot!</button>
   );
 }
+function Football2() {
+  const shoot = (a, b) => {
+    alert(b.type);
+    /*
+    'b' represents the React event that triggered the function,
+    in this case the 'click' event
+    */
+  }
+
+  return (
+    <button onClick={(event) => shoot("Goal!", event)}>Take the shot!</button>
+  );
+}
 root.render(
   <React.StrictMode>
-    <Football />
+    <Football2/>
   </React.StrictMode>
 );
 
